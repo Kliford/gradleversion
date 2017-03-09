65c9ce021d69bf1b2e730a799c729e6212254818
sar0045 <sarlatan@seznam.cz>
2017-03-09 18:28:43 +0200

    test2 commit

--- app/src/main/java/com/kru13/httpserver/HttpServerActivity.java
+++ app/src/main/java/com/kru13/httpserver/HttpServerActivity.java
@@ -338,7 +338,7 @@ public class HttpServerActivity extends Activity implements OnClickListener{
 		if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
 			for (String permission : permissions) {
 				if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
-					return false;///
+					return false;/////
 				}
 			}
 		}
